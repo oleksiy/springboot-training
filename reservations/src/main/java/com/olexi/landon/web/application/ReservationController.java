5cdc6a179d9a287a6da272d68ac67e69b44b8a62
Oleksiy Blavat <blavat.alex@gmail.com>
2017-04-08 17:15:00 -0400

    final touches'

--- reservations/src/main/java/com/olexi/landon/web/application/ReservationController.java
+++ reservations/src/main/java/com/olexi/landon/web/application/ReservationController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**

f047af7861abaf1695412d839ae4aa57778bd9d7
Oleksiy Blavat <blavat.alex@gmail.com>
2017-04-08 16:35:17 -0400

    test fails for reservations

--- reservations/src/main/java/com/olexi/landon/web/application/ReservationController.java
+++ reservations/src/main/java/com/olexi/landon/web/application/ReservationController.java
@@ -1,8 +1,19 @@
 package com.olexi.landon.web.application;
 
+import com.olexi.landon.business.domain.RoomReservation;
+import com.olexi.landon.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by oblavat on 4/8/17.
@@ -10,9 +21,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/reservations")
 public class ReservationController {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    private ReservationService reservationService;
 
     @RequestMapping(method= RequestMethod.GET)
-    public String getReservations(){
+    public String getReservations(@RequestParam(value="date", required=false)String dateString, Model model){
+        Date date = null;
+        if(null != dateString){
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            } catch (ParseException pe) {
+                date = new Date();
+            }
+        }else{
+            date = new Date();
+        }
+
+        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservationList);
         return "reservations";
     }
 }
